c11016413cd271352f5909f942ba2b2a3c918955
n3ndo <ferdinandof3lixbagnale@gmail.com>
2024-02-15 17:07:36 +0100

    [UPDATE] created train migration and seeder

--- database/migrations/2024_02_15_154749_create_trains_table.php
+++ database/migrations/2024_02_15_154749_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 100);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 10);
+            $table->tinyInteger('carriages');
+            $table->boolean('is_on_time')->default(true);
+            $table->boolean('is_deleted')->default(false);
             $table->timestamps();
         });
     }
