4087adbb75fe3ca45028f59f10836677859c6c57
n3ndo <ferdinandof3lixbagnale@gmail.com>
2024-02-15 18:25:03 +0100

    [UPDATE] populated table rows

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -14,6 +15,25 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+
+        // Modifica il numero di righe desiderato
+        $numberOfRows = 20;
+
+        for ($i = 0; $i < $numberOfRows; $i++) {
+            DB::table('trains')->insert([
+                'company' => $faker->company,
+                'departure_station' => $faker->city,
+                'arrival_station' => $faker->city,
+                'departure_time' => $faker->time('H:i:s'),
+                'arrival_time' => $faker->time('H:i:s'),
+                'train_code' => $faker->unique()->bothify('??###??'),
+                'carriages' => $faker->numberBetween(1, 5),
+                'is_on_time' => $faker->boolean,
+                'is_deleted' => $faker->boolean,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
